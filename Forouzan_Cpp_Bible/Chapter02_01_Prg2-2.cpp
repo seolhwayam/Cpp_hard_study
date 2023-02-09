611540d388ee084a48bd93bc0e6a9f73a53befe0
study <susung462@gmail.com>
2023-02-10 00:15:22 +0900

    Chapter02_01_Prg2-2

---
+++ Forouzan_Cpp_Bible/Chapter02_01_Prg2-2.cpp
@@ -0,0 +1,19 @@
+//
+// Created by rudnf on 2023-02-10.
+//
+// * 별로 사각형을 출력하는 프로그램
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"******" <<endl;
+    cout<<"******" <<endl;
+    cout<<"******" <<endl;
+    cout<<"******" <<endl;
+    cout<<"******" <<endl;
+    cout<<"******" <<endl;
+    return 0;
+
+}
+
