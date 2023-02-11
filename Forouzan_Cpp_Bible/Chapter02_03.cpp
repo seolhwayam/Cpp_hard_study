3190c49206caa0c96e30e9b261ff666728f9a0bd
study <susung462@gmail.com>
2023-02-12 02:40:11 +0900

    Chapter02_03

---
+++ Forouzan_Cpp_Bible/Chapter02_03.cpp
@@ -0,0 +1,14 @@
+//
+// Created by rudnf on 2023-02-12.
+//
+#include <iostream
+using namespace std;
+int main(){
+    /* <토큰과 주석>
+     * 1. 토큰 : 실행 가능한 코드(주석제외 코드)
+     *    식별자 : 엔티티의 이름(기호/글자/_ 로 시작해야됨)
+     *
+     * 2. 리터럴 : 자료형을 가진 상수값
+
+     */
+}
