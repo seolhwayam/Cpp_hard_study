df5e89e09cbede0f2ea2e7a90bf012bcc1b8aaa1
study <susung462@gmail.com>
2023-02-12 02:34:09 +0900

    Chapter02_02_Prg2-3

--- Forouzan_Cpp_Bible/Chapter02_02_Prg2-3.cpp
+++ Forouzan_Cpp_Bible/Chapter02_02_Prg2-3.cpp
@@ -23,6 +23,27 @@ int main()
     // 계산과 결과 저장
     sum = num1+num2;
     //출력
-    cout << "두 숫자의 합:"<<sum;
+    cout << "두 숫자의 합:"<<sum <<endl;
+
+    // 상수
+    int radius;
+    const int PI = 3;
+    cout<<"반지름 길이 :";
+    cin >>radius;
+    int perimeter = 2 * PI * radius;
+    cout << "원의 지름 :";
+    cout << perimeter;
     return 0;
+
+    /* 입력받기  cin >> "변수"
+     * 출력하기  cout << 값
+
+     * 할당연산자 (=)
+
+     * 상수 (const)
+     * : 값을 변경할 수 없는 저장소 엔티티
+
+     * 리터럴
+     * : 메모리에 저장되지 않고 활용할 수 있는 값
+     */
 }
