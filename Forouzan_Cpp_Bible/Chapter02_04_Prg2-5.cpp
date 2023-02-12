95682071a19d966bd2611338a7086c9000a2bcbf
study <susung462@gmail.com>
2023-02-12 16:57:05 +0900

    Chapter02_04_Prg2-5

---
+++ Forouzan_Cpp_Bible/Chapter02_04_Prg2-5.cpp
@@ -0,0 +1,32 @@
+//
+// Created by rudnf on 2023-02-12.
+//
+#include <iostream>
+using namespace std;
+// 3회 거래 후의 계좌 잔액을 구하는 프로그램
+
+
+int main(){
+    //변수 선언
+    int balance =0;
+    int transaction;
+
+    //첫 번째 거래 후 잔액 조정
+    cout << "첫 번째 거래 금액 입력 : ";
+    cin >> transaction;
+    balance = balance + transaction;
+
+    //두 번째 거래 후 잔액 조정
+    cout << "두 번째 거래 금액 입력 : ";
+    cin >> transaction;
+    balance = balance + transaction;
+
+    //세 번째 거래 후 잔액 조정
+    cout << "세 번째 거래 금액 입력 : ";
+    cin >> transaction;
+    balance = balance + transaction;
+
+    //최종 잔액 출력
+    cout << "계좌의 최종 잔액은"<<balance<<"달러입니다.";
+    return 0;
+}
