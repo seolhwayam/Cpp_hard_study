acc8a7545f5908efb61b12ae8664a1cf61627533
study <susung462@gmail.com>
2023-02-12 17:28:49 +0900

    Chapter02_04_Prg1-6

---
+++ Forouzan_Cpp_Bible/Chapter02_04_Prg2-6.cpp
@@ -0,0 +1,14 @@
+//
+// Created by rudnf on 2023-02-12.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+    // 3가지 정수 자료형의 크기를 확인하는 프로그램
+    cout << "shot int의 크기는" << sizeof(short int) << "바이트입니다."<<endl;
+    cout << "int의 크기는" << sizeof(int) << "바이트입니다." <<endl;
+    cout << "long int의 크기는" << sizeof(long int) << "바이트입니다."<<endl;
+    return 0;
+
+}
