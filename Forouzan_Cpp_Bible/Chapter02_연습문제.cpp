6fe56724ccfb72f1805ed954082cf9fb2aa6475e
study <susung462@gmail.com>
2023-02-15 01:01:52 +0900

    Chapter02_연습문제

---
+++ Forouzan_Cpp_Bible/Chapter02_연습문제.cpp
@@ -0,0 +1,28 @@
+//
+// Created by rudnf on 2023-02-15.
+//
+# include <iostream>
+using namespace std;
+
+int main(){
+    //Chapter02 연습문제
+
+    // 프로그래밍 문제 01
+    cout<<"*\n**\n***" <<endl;
+
+    cout << "---------------------------"<<endl;
+
+    // 프로그래밍 문제 02
+    cout<<"*\n**\n***\n**\n*";
+
+    cout << "---------------------------"<<endl;
+
+    // 프로그래밍 문제 03
+    cout<<"H     H\nH     H\nHHHHHHH\nH     H\nH     H";
+    return 0;
+
+    // 프로그래밍 문제 04  : 시간, 분, 초 단위로 시간을 입력받아서 초 단위로 환산해 출력하는 프로그램
+    int
+
+
+}
