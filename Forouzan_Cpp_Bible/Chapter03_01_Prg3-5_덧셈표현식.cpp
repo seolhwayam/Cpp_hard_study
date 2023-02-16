b99f96fa8689001fdf56a5e9dee8a55a6c9a47e3
study <susung462@gmail.com>
2023-02-17 00:03:06 +0900

    Chapter03_01_Prg3-5_덧셈표현식

---
+++ Forouzan_Cpp_Bible/Chapter03_01_Prg3-5_덧셈표현식.cpp
@@ -0,0 +1,24 @@
+//
+// Created by rudnf on 2023-02-16.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*µ¡¼À Ç¥Çö½Ä - µ¡¼À »¬¼À Ç¥Çö½Ä È®ÀÎÇÏ±â*/
+
+    //µ¡¼À ¿¬»êÀÚ
+    cout << "µ¡¼À ¿¬»êÀÚ È®ÀÎÇÏ±â "<<endl;
+    cout << "30 + 5 = "<<30+5<<endl;
+    cout << "20.5 + 6.2 = "<<20.5+6.2 <<endl;
+
+    //»­¼À ¿¬»êÀÚ
+    cout << "»¬¼À ¿¬»êÀÚ È®ÀÎÇÏ±â "<<endl;
+    cout << "5 - 30 = "<<5-30<<endl;
+    cout << "51.2 - 30.4 = "<<51.2-30.4 <<endl;
+
+    /*4.µ¡¼À Ç¥Çö½Ä
+     *  : ¿ÞÂÊ°ú ¿À¸¥ÂÊ¿¡ ÇÇ¿¬»êÀÚ°¡ ÀÖ´Â ÀÌÇ× Ç¥Çö½Ä
+     *  : µ¡¼À°ú »¬¼À ¿¬»êÀÚ Æ÷ÇÔÇÏ´Â Ç¥Çö½Ä*/
+    return 0;
+}
