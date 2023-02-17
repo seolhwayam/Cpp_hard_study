a60861deaeecf3521ef7061cb12e5cf16bb1dc4c
study <susung462@gmail.com>
2023-02-17 23:58:16 +0900

    Chapter03_01_Prg3-6_할당표현식

--- Forouzan_Cpp_Bible/Chapter03_01_Prg3-6_할당 표현식.cpp
+++ Forouzan_Cpp_Bible/Chapter03_01_Prg3-6_할당 표현식.cpp
@@ -6,5 +6,15 @@ using namespace std;
 
 int main(){
     /*단순 할당 연산자 - 단순 할당 연산자 확인하기*/
+
+    // 변수 선언
+    int x;
+    int y;
+
+    // 첫 번째 할당
+    cout << "할당 표현식의 리턴값 : " << (x=14)<<endl;
+    cout << "변수 X의 값 : " << x << endl;
+
+
     return 0;
 }
