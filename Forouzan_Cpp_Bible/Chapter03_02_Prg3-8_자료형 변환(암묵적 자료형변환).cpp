cbb34922e19fd09a13b13e2fb8bc962b42e8895a
study <susung462@gmail.com>
2023-02-19 01:30:04 +0900

    Chapter03_02_Prg3-8_자료형변환(암묵적 자료형변환)

---
+++ Forouzan_Cpp_Bible/Chapter03_02_Prg3-8_자료형 변환(암묵적 자료형변환).cpp
@@ -0,0 +1,21 @@
+//
+// Created by rudnf on 2023-02-19.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*암묵적 자료형 승격
+     * - 산술 연산자가 없는 자료형(bool,char,short,float)
+     * - 산술 연산자를 적용해서 암묵적 자료형 변환하기       */
+
+    //선언
+    bool x = true;
+    char y = 'A';
+    short z = 14;
+    float t = 24.5;
+
+    //bool에서 int로 자료형 변환
+
+    return 0;
+}
