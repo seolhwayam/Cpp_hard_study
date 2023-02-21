3223544f2f37cd5312c08990a9999a87bc1a5e66
study <susung462@gmail.com>
2023-02-21 23:50:21 +0900

    Chapter03_05_Prg-21,22,23_데이터 형식 조정_출력조정자(매개변수x)

---
+++ Forouzan_Cpp_Bible/Chapter03_05_Prg21,22,23_데이터 형식 조정_출력조정자(매개변수 x).cpp
@@ -0,0 +1,24 @@
+//
+// Created by rudnf on 2023-02-21.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    /*boolalpha 조정자 사용하기
+     * : 불 자료형에 boolalpha 조정자 사용하기*/
+
+    //선언
+    bool x = true;
+    bool y = false;
+
+    //조정자 없이 출력
+    cout << "기본적인 x의 출력 : "<<x <<endl;
+    cout << "기본적인 y의 출력 : "<<y <<endl;
+
+    //조정자를 사용하여 출력
+    cout << "조정자를 사용한 x의 값 : "<<boolalpha<<x<<endl;
+    cout << "y의 값 : "<<y;
+    return 0;
+}
