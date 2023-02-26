41435fc33ff2e34e0afdc7e2746a35c8db292510
study <susung462@gmail.com>
2023-02-26 23:50:32 +0900

    Chapter04_01_Prg4-1,2_if조건문

---
+++ Forouzan_Cpp_Bible/Chapter04_01_Prg4-1,2_if조건문.cpp
@@ -0,0 +1,31 @@
+//
+// Created by rudnf on 2023-02-26.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*절대값 출력하기
+     * : if 조건문을 사용해서 절대값을 계산하고 출력하는 프로그램*/
+
+    //변수 선언
+    int number ;
+
+    //입력받기
+    cout << "정수를 입력하세요 : ";
+    cin >> number;
+
+    //절대값 찾기
+    if(number < 0){
+        number = -number;
+    }
+
+    //절대값 출력
+    cout <<"입력한 숫자의 절대값 = "<<number;
+
+
+
+
+    return 0;
+}
