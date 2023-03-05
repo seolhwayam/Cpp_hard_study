dd84427b46f43799aa951540cb7b0e170c05d663
study <susung462@gmail.com>
2023-03-06 00:08:36 +0900

    Chapter05_02_Prg5-1,2,3,4,5,6,7,8_While반복문

---
+++ Forouzan_Cpp_Bible/Chapter05_02_Prg5-1,2,3,4,5,6,7,8_While반복문.cpp
@@ -0,0 +1,22 @@
+//
+// Created by rudnf on 2023-03-06.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*while반복문으로 특정 수 만큼 반복하기1
+     *  : while반복문을 사용해서 메시지를 10회 출력하기*/
+
+    // 카운터 선언하고 초기화
+    int counter =0;
+
+    //while반복문
+    while(counter<10){
+        cout <<"Hello world!"<<endl;
+        counter++;
+    }
+
+    return 0;
+}
