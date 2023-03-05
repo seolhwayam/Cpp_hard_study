b87015c1db548f5895ab3482288050cd4900b8a4
study <susung462@gmail.com>
2023-03-05 23:58:51 +0900

    Chapter05_01_반복문 개요

---
+++ Forouzan_Cpp_Bible/Chapter05_01_반복문 개요.cpp
@@ -0,0 +1,18 @@
+//
+// Created by rudnf on 2023-03-05.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*후위 표현식과 전위 표현식
+     * 후위 value++ | value--  →
+     * 전위 ++value | --value  ←
+     *
+     * ① 변수의 값은 같은 결과 나옴
+     * ② 차이점 : 리턴값
+     *   - 후의 표현식 : 원래 값을 리턴
+     *   - 전의 표현식 : 부가 작용이 일어난 뒤의 값을 리턴*/
+
+    return 0;}
