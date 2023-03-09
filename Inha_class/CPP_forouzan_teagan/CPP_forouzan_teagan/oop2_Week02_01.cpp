5a3e94e3ec6f3688ddec6867639d0146168a2950
study <susung462@gmail.com>
2023-03-10 03:27:31 +0900

    oop2_Week02_01_포인터

--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week02_01.cpp
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week02_01.cpp
@@ -16,11 +16,38 @@ int main() {
 	cout << s << endl;
 
 	//배열
-	int arr[4];
-	arr[0] = -9;
-	arr[3] = 30000;
+	//int arr[4] = { 0 };
+	//① 배열 0,1,2,3 0으로 초기화 | {초기화 값}
+	//arr[0] = -9;
+	//arr[3] = 30000;
 
-	cout << arr[3] << endl;
+	int arr[4] = { -9,0,0,30000 };
+	//② 선언시에 초기화
+
+
+	int* parr = &arr[0];
+	//포인터 = 담긴 주소(*)
+	//parr=포인터 이름 | &arr[0] = 0번방의 번지수
+	//타입* 포인터이름 = &배열이름[];
+	//▶ 배열명은 포인터 상수다
+
+
+
+	for (int i = 0; i < 4; i++) {
+		//cout <<*(parr+i)<<endl; ①포인터| 주소로 값 출력
+		//cout << parr[i] <<endl; ②포인터| 포인터 배열처럼 활용
+		//cout << arr[i] <<endl;  ③배열|배열 출력
+		cout << *(arr + i) << endl;
+	}
+
+	//for (int i = 0; i < 4; i++) {
+	//	cout << parr+i << endl;
+	//}//자리주소는 그대로지만 4바이트씩 증가(parr+i)
+	   //주소 출력하기
+
+
+
+	cout << arr[2] << endl;
 
 	return 0;
 
