8753fa4563d28f4c863ab83f81ad76360a7eec78
study <susung462@gmail.com>
2023-03-13 21:28:01 +0900

    Chapter05_05_Prg5-14,15,16_중첩반복문

--- Forouzan_Cpp_Bible/Chapter05_05_Prg5-14,15,16_중첩반복문.cpp
+++ Forouzan_Cpp_Bible/Chapter05_05_Prg5-14,15,16_중첩반복문.cpp
@@ -11,19 +11,42 @@ int main(){
     cout << "Prg 5-14 for 반복문 중첩하기"<<endl;
 
     // 선언
-    int rows;
-    int cols;
+    int rows_01;
+    int cols_01;
 
     // 입력받기
     cout << "행의 수를 입력하세요: ";
-    cin >> rows;
+    cin >> rows_01;
     cout << "열의 수를 입력하세요: ";
-    cin >> cols;
+    cin >> cols_01;
 
     // 출력
-    for (int count1 =1; count1 <=rows; count1++){
-        for(int count2=1; count2<=cols; count2++){
+    for (int count1 =1; count1 <= rows_01; count1++){
+        for(int count2=1; count2 <= cols_01; count2++){
             cout <<"*";
+        }
+        cout <<endl;
+    }
+
+    /* 숫자 패턴 출력하기
+     *  : 반복문을 중첩해서 숫자를 정해진 패턴으로 출력하는 프로그램*/
+
+    cout << "Prg 5-15 숫자 패턴 출력하기"<<endl;
+
+    // 선언
+    int rows_02;
+    int cols_02;
+
+    // 입력받기
+    cout << "행의 수를 입력하세요: ";
+    cin >>rows_02;
+    cout <<"열의 수를 입력하세요: ";
+    cin >>cols_02;
+
+    // 중첩 반복문
+    for(int i=1;i<=rows_02;i++){
+        for(int j =i; j<=i+cols_02-1;j++){
+            cout <<j<<" ";
         }
         cout <<endl;
     }
