9d279229b141054f59bd17ca906822061ce422aa
study <susung462@gmail.com>
2023-03-13 21:35:20 +0900

    Chapter05_05_Prg5-14,15,16_중첩반복문

--- Forouzan_Cpp_Bible/Chapter05_05_Prg5-14,15,16_중첩반복문.cpp
+++ Forouzan_Cpp_Bible/Chapter05_05_Prg5-14,15,16_중첩반복문.cpp
@@ -2,6 +2,7 @@
 // Created by rudnf on 2023-03-13.
 //
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
@@ -50,6 +51,27 @@ int main(){
         }
         cout <<endl;
     }
+
+    /*곱셉 표 만들기
+     *  : 반복문을 중첩해서 2~10범위를 갖는 크기의 곱셈 표를 출력하는 프로그램*/
+
+    // 변수 size 선언
+    int size;
+
+    // 입력받고 유효성 검사
+    do{
+        cout <<"표의 크기를 입력하세요(2~10): ";
+        cin >> size;
+    }while(size <2 ||size >10);
+
+    // 중첩 반복문으로 표 출력
+    for(int i =1; i<=size; i++){
+        for(int j =1; j<=size; j++){
+            cout <<setw(4)<<i*j;
+        }
+        cout <<endl;
+    }
+
 
     return 0;
 }
