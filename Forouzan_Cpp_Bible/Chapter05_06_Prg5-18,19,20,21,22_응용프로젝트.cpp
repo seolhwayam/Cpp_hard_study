aeaaf06d4c4f70fd14cb462dfd4b86b7cdb8e7b9
study <susung462@gmail.com>
2023-03-15 22:23:29 +0900

    Chapter05_06_Prg5,18,19,20,21,22_응용프로젝트

---
+++ Forouzan_Cpp_Bible/Chapter05_06_Prg5-18,19,20,21,22_응용프로젝트.cpp
@@ -0,0 +1,14 @@
+//
+// Created by rudnf on 2023-03-15.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*소수인지 확인하는 프로그램
+     *  : 소수를 찾았을 때 return 구문을 실행하는 프로그램
+     *    어떤 숫자가 1인지 또는 복합수인지 또는 소수인지
+     *    확인하면 곧바로 main 함수를 벗어남*/
+
+    return 0;
+}
