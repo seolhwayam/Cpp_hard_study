ba9ee92b4e1e14efdc996c0c6b9ee653631f20d9
study <susung462@gmail.com>
2023-03-31 01:09:51 +0900

    oop2_Week05_02(람다2)

--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week05_02(람다2).cpp
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week05_02(람다2).cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+	// ▶ 캡처블럭 값 변경 오류
 	//short c = 5, d = 7;
 	//auto inha_1 = [c, d](float a, int b)-> int { //[c,d] 값으로서만 캡처 >> 그래서 수정 X 값에다 어캐함;;
 	//	return a + b + c + d;
@@ -9,13 +10,32 @@ int main() {
 
 	//cout << inha_1(1.9f, 2) << endl;
 
+
+
+
+	// ① 캡처블럭 값 변경하기
+	// 
+	//캡쳐블럭 : 외부 변수 참조 가능
+	// 
+	// 1) [&변수] : & + 특정변수 그 변수만 참조형태로 해서 원본의 값을 변경
+	
 	short c = 5, d = 7;
-	auto inha_2 = [&c, d](float a, int b)-> int { 
+	auto inha_2 = [&c, &d](float a, int b)-> int {  
 		c = -5;
+		d = 8;
 		return a + b + c + d;
 	};
 
 	cout << inha_2(1.9f, 2) << endl;
+
+	// 2) [&] : 전체, 모든 변수에 대한 참조로써 값을 변경 
+	auto inha_3 = [&](float a, int b)-> int {  
+		c = -5;
+		d = 8;
+		return a + b + c + d;
+	};
+
+	cout << inha_3(1.9f, 2) << endl;
 
 
 	return 0;
