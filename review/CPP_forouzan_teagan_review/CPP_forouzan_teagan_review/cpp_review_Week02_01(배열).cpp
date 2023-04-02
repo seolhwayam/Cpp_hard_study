5fd66b7c8737cacd5b384cdf7e54c445882ed1e2
study <susung462@gmail.com>
2023-04-03 02:37:20 +0900

    cpp_review_Week02_01(포인터 배열)

--- review/CPP_forouzan_teagan_review/CPP_forouzan_teagan_review/cpp_review_Week02_01(배열).cpp
+++ review/CPP_forouzan_teagan_review/CPP_forouzan_teagan_review/cpp_review_Week02_01(배열).cpp
@@ -1,7 +1,7 @@
-#include<iostream>
-using namespace std;
-
-int main() {
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
 	// ② 배열
 
 	//1) 초기화 안했을 경우 에러
@@ -12,12 +12,21 @@ int main() {
 	//cout << arr[3] << endl; //30000
 	//cout << arr[2] << endl; // 더미값(쓰레기값) 출력
 
-	int arr[4] = { 0 };
-	arr[0] = -9;
-	arr[3] = 30000;
 
-	cout << arr[3] << endl; //30000
-	cout << arr[2] << endl; // 더미값(쓰레기값) 출력
+	//2) 배열 생성 시 셋팅01
+	//int arr[4] = { 0 };
+	//arr[0] = -9;
+	//arr[3] = 30000;
+
+	//cout << arr[3] << endl; //30000
+	//cout << arr[2] << endl; // 0
+
+	//for (int i = 0; i < 4; i++) {
+	//	cout << arr[i] << endl;
+	//}
+	
+	//3) 배열 생성 시 셋팅02 (생성과 동시에 초기화)
+	/*int arr[4] = { -9,0,0,3000 };
 
 	for (int i = 0; i < 4; i++) {
 		cout << arr[i] << endl;
@@ -29,4 +38,4 @@ int main() {
 
 
 	return 0;
-}
+}*/
