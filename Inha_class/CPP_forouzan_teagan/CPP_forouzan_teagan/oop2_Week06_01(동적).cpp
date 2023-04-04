4f8418130956347941e008cd57174466c0a6756d
study <susung462@gmail.com>
2023-04-05 04:35:05 +0900

    oop2_Week06_01(동적 메로리 할당2)

---
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week06_01(동적).cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#pragma warning(disable:4996)
+#include<cstring> //strlen 함수 사용(옛날 버전시에는 필수 입력)
+using namespace std;
+
+int main() {
+
+	// ① 문자열 길이
+	char src1[] = "Inha";
+	int len1 = strlen(src1);
+	// strlen : 문자열 길이 재는 함수
+
+	cout << len1 << endl; //4
+
+	char src2[] = "Inha Univ";
+	int len2 = strlen(src2);
+
+	cout << len2 << endl; //9(스페이스바도 1로 인식)
+
+	// ② 문자열 복사
+	char* dest = new char[len1 + 1]; // 동적 메모리 할당
+	strcpy(dest, src1);
+	// 첫번째 인자 : 복사를 할 곳 | 두 번째 인자 : 원본
+
+	cout << src1 << endl;
+	cout << dest << endl;
+
+
+	delete[] dest; // 동적 메모리 할당 해제
+	dest = NULL; // 포인터 삭제
+
+
+	return 0;
+}
--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week06_01(동적메모리 할당).cpp
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week06_01(동적메모리 할당).cpp
@@ -1,47 +1,47 @@
-#include<iostream>
-using namespace std;
-
-char* ReverseString(const char* src, int len) {
-	/*  ① const + char*(포인터) : 원본의 값을 바꾸지 못함(읽기 전용)
-	*   ② char* + src : 메모리 번지 받음. 주소(orig -> Inha)
-	*   ③ int + len : 문자열의 갯수
-	*   src -> orig -> []Inha
-	*   len -> 4
-   */
-	char* rev = new char[len + 1]; 
-	//동적메모리 할당 5바이트
-	// cout  출력시 null전까지만 출력, cout에게 알려주기 위해서 null값 넣음 -> null값 처리 위해 5바이트
-
-
-	for (auto i = 0; i < len; ++i) {
-		rev[i] = src[len - i - 1];
-		// src포인터 -> orig[] 배열 주소 -> rev포인터 배열
-	}
-	rev[len] = NULL;
-	// cout  출력시 null전까지만 출력, cout에게 알려주기 위해서 null값 넣음
-	// 관련해서 char* rev = new char[len + 1] -> null값 처리위해 5바이트
-
-	return rev;
-	}
-
-int main() {
-	char orig[] = "Inha";
-	// 문자열 배열 Inha +null 들어가 있음
-	// orig[] = [ I , n , h , a , null(0) ]
-
-	char* copy = ReverseString(orig, 4); // 함수 호출
-	cout << orig << "\n";
-	cout << copy << "\n";
-
-
-	//★ 중요 같이 사용!
-	delete[] copy;
-	// [] = [ a,  h  , n , I ,  null] 삭제
-
-	copy = NULL;
-	//포인팅X 하도록 설정
-
-
-
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//char* ReverseString(const char* src, int len) {
+//	/*  ① const + char*(포인터) : 원본의 값을 바꾸지 못함(읽기 전용)
+//	*   ② char* + src : 메모리 번지 받음. 주소(orig -> Inha)
+//	*   ③ int + len : 문자열의 갯수
+//	*   src -> orig -> []Inha
+//	*   len -> 4
+//   */
+//	char* rev = new char[len + 1]; 
+//	//동적메모리 할당 5바이트
+//	// cout  출력시 null전까지만 출력, cout에게 알려주기 위해서 null값 넣음 -> null값 처리 위해 5바이트
+//
+//
+//	for (auto i = 0; i < len; ++i) {
+//		rev[i] = src[len - i - 1];
+//		// src포인터 -> orig[] 배열 주소 -> rev포인터 배열
+//	}
+//	rev[len] = NULL;
+//	// cout  출력시 null전까지만 출력, cout에게 알려주기 위해서 null값 넣음
+//	// 관련해서 char* rev = new char[len + 1] -> null값 처리위해 5바이트
+//
+//	return rev;
+//	}
+//
+//int main() {
+//	char orig[] = "Inha";
+//	// 문자열 배열 Inha +null 들어가 있음
+//	// orig[] = [ I , n , h , a , null(0) ]
+//
+//	char* copy = ReverseString(orig, 4); // 함수 호출
+//	cout << orig << "\n";
+//	cout << copy << "\n";
+//
+//
+//	//★ 중요 같이 사용!
+//	delete[] copy;
+//	// [] = [ a,  h  , n , I ,  null] 삭제
+//
+//	copy = NULL;
+//	//포인팅X 하도록 설정
+//
+//
+//
+//	return 0;
+//}
