cfa5b7f438a0948e8accb545e17242e8fc613f7f
study <susung462@gmail.com>
2023-04-07 01:20:12 +0900

    oop2_Week06_03(헤더파일, 소스파일 분리)

---
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week06_03(헤더파일, 소스파일 분리).cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "oop2_Week06_03_ex1.h" //사용자 정의 파일
+using namespace std;
+
+int main() {
+	int r;
+	r = Plus(3, 7);
+
+	return 0;
+}
---
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week06_03_ex1.h.cpp
@@ -0,0 +1,4 @@
+#pragma once // 중복파일 
+int Plus(int a, int b) { // 프로토 타입
+	return a + b;
+}
