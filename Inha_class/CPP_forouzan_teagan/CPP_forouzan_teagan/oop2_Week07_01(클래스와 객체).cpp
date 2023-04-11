c11790326d125eb8744488df03f940f9dd3d9d4c
study <susung462@gmail.com>
2023-04-12 01:59:38 +0900

    oop2_Week07_01(생성자)

--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week07_01(클래스와 객체).cpp
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week07_01(클래스와 객체).cpp
@@ -1,18 +1,18 @@
-#include<iostream>
-using namespace std;
-
-/*구조체와 클래스
-	  ① class 키워드 사용
-	  ② pubilc 과 같은 접근제어와 관련된 키워드 사용
-	  ③ 맴버함수(메서드)를 정의할 수 있다.	 (메서드는 클래스에 선언된 함수)
-
-	  구조체
-	  ① struct 키워드 사용
-	  ② pubilc  접근제어
-	  ③ 맴버변수를 정의할 수 있다.
-	  */
-
- 
+//#include<iostream>
+//using namespace std;
+//
+///*구조체와 클래스
+//	  ① class 키워드 사용
+//	  ② pubilc 과 같은 접근제어와 관련된 키워드 사용
+//	  ③ 맴버함수(메서드)를 정의할 수 있다.	 (메서드는 클래스에 선언된 함수)
+//
+//	  구조체
+//	  ① struct 키워드 사용
+//	  ② pubilc  접근제어
+//	  ③ 맴버변수를 정의할 수 있다.
+//	  */
+//
+// 
 //class Point {
 //public:
 //	int x, y;
@@ -40,7 +40,7 @@ using namespace std;
 //
 //	return 0;
 //}
-
-
-
-
+//
+//
+//
+//
