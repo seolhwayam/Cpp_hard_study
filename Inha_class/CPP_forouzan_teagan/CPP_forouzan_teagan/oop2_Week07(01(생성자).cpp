f3e90b2f188b4b268b246104ff2fa10defee0c99
study <susung462@gmail.com>
2023-04-13 23:53:02 +0900

    oop2_Week07_02(복사생성자)

--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week07(01(생성자).cpp
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week07(01(생성자).cpp
@@ -1,50 +1,50 @@
-#include<iostream>
-using namespace std;
- 
-class Point {
-public:
-	int x, y;
-	Point(); // 생성자
-	Point(int _x, int _y); // 생성자 오버로딩(매개변수 버전)
-	void Print();
-
-};
-Point::Point() { // ①생성자는 리턴타입이 없다 ②클래스의 이름과 같다
-	x = 0;
-	y = 0;
-}
-Point::Point(int _x,int _y) {
-	x = _x;
-	y = _y;
-}
-// 생성자 : 객체를 생성할 때 자동으로 호출되는 함수(객체 생성시 딱 한 번 생성)
-//		    ①생성자는 리턴타입이 없다 ②클래스의 이름과 같다
-// 소멸자 : 객체가 소멸될 때 자동으로 호출되는 함수
-
-void Point::Print() {
-	cout << x << "," << y << endl;
-};
-
-int main() {
-	Point p1, p2 ,p4 ,p5(200,150); // 객체 생성
-	p1.x = 10;    // 맴버변수 초기화
-	p1.y = 10;
-	p2.x = 20;
-	p2.y = 20;
-
-	Point p3 = p2; //초기화
-	p3.Print();
-	p3 = p1; // 대입
-	
-	p3.Print();
-	p4.Print();
-	p5.Print();
-
-	p1.Print(); //클래스 내 메서드 사용시 
-	
-	return 0;
-}
-
-
-
-
+//#include<iostream>
+//using namespace std;
+// 
+//class Point {
+//public:
+//	int x, y;
+//	Point(); // 생성자
+//	Point(int _x, int _y); // 생성자 오버로딩(매개변수 버전)
+//	void Print();
+//
+//};
+//Point::Point() { // ①생성자는 리턴타입이 없다 ②클래스의 이름과 같다
+//	x = 0;
+//	y = 0;
+//}
+//Point::Point(int _x,int _y) { // 생성자 오버로딩
+//	x = _x;
+//	y = _y;
+//}
+//// 생성자 : 객체를 생성할 때 자동으로 호출되는 함수(객체 생성시 딱 한 번 생성)
+////		    ①생성자는 리턴타입이 없다 ②클래스의 이름과 같다
+//// 소멸자 : 객체가 소멸될 때 자동으로 호출되는 함수
+//
+//void Point::Print() {
+//	cout << x << "," << y << endl;
+//};
+//
+//int main() {
+//	Point p1, p2 ,p4 ,p5(200,150); // 객체 생성
+//	p1.x = 10;    // 맴버변수 초기화
+//	p1.y = 10;
+//	p2.x = 20;
+//	p2.y = 20;
+//
+//	Point p3 = p2; //초기화
+//	p3.Print();
+//	p3 = p1; // 대입
+//	
+//	p3.Print();
+//	p4.Print();
+//	p5.Print(); // 생성자 오버로딩 버전
+//
+//	p1.Print(); //클래스 내 메서드 사용시 
+//	
+//	return 0;
+//}
+//
+//
+//
+//
---
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week07_02(복사생성자).cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//
+//class point {
+//public:
+//	int x, y;
+//
+//	void print();
+//
+//};
+//
+//void point::print() { // 어느 클래스에 속한 메소드인지 
+//	cout << x << "," << y << endl;
+//}
+//
+///*1. 클래스 정의 안에는 메소드의 프로토타입만 남겨둔다.
+//  2. 클래스 정의 밖에서는 범위 지정 연산자를 사용해서 함수를 정의(void point::print())*/
+//
+//int main() {
+//	point p1, p2; // 객체 생성
+//	p1.x = 10;    // 맴버변수 초기화
+//	p1.y = 10;
+//	p2.x = 20;
+//	p2.y = 20;
+//
+//	p1.print(); //클래스 내 메서드 사용시 
+//	
+//
+//
+//
+//	return 0;
+//}
