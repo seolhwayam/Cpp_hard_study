675a1c855b7bb1999929c7c3a5a99d0fa79c6748
study <susung462@gmail.com>
2023-04-14 02:03:00 +0900

    oop2_Week07_03(접근제어)

---
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week07_03(접근제어).cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class AccessControl {
+public:
+	char publicDate;
+	void publicFunc() {};
+
+/*public: 모든 외부의 접근 허용*/
+
+protected:
+	int protectedDate;
+	void protectedFunc() {};
+private:
+	float privateDate;
+	void privateFunc() {};
+
+public:
+	void AccesssAllMembers();
+
+/*접급제한자 미선언시 기본적으로 private 동작*/
+
+};
+void  AccessControl::AccesssAllMembers() {
+	publicDate = 'A';
+	publicFunc();
+
+	protectedDate = 100;
+	protectedFunc();
+
+	privateDate = 4.5f;
+	privateFunc();
+
+}
+
+int main() {
+	AccessControl ac;
+	ac.AccesssAllMembers();
+
+	//ac.publicDate = 'A';     //성공
+	//ac.publicFunc();         //성공
+	//
+	//ac.protectedDate = 100;  //실패
+	//ac.protectedFunc();      //실패
+
+	//ac.privateDate = 4.5f;   //실패
+	//ac.privateFunc();        //실패
+}
