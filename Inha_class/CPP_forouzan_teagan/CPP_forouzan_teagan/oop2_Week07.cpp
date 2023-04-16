e86087acca4cc4ee91ea3151decf6ae0052d8709
study <susung462@gmail.com>
2023-04-16 20:41:33 +0900

    oop2_Week07

--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week07.cpp
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week07.cpp
@@ -3,7 +3,7 @@
  * 문자열 전체 또는 문자열 일부를                             *
  * 다른 문자열로 복사하는 프로그램                            *
  **************************************************************/
-
+#pragma warning(disable:4996);
 #include <cstring> //c언어에서 사용하는 함수는 c가 붙는다.
 #include<string> // c++스타일
 #include <iostream>
@@ -16,8 +16,23 @@ int myLen(const char* a) {
 	}
 	return count;
 
-
 };
+
+char* mystrcat(char s1[],const char* s2) {
+	int startlen = strlen(s1);
+	int k = 0;
+	int i = startlen;
+	for (; s2[k] != NULL; i++) {
+		s1[i] = s2[k];
+		k++;
+	}
+	s1[i] = NULL;
+
+
+	return s1;
+}
+
+
 
 int main()
 {
@@ -41,9 +56,51 @@ int main()
 	//a = b;
 	//cout << a << endl;
 
-	char a[] = "inha";
+	char a[20] = "inha";
+	char b[] = "ho";
 
-	cout << myLen(a);
 
-	return 0;
+	cout << myLen(a) << endl;
+	cout << mystrcat(a, b);
 }
+
+
+
+	/*
+	#include <iostream>
+using namespace std;
+
+int main(){
+    char str1[6] = {'H', 'e', 'l', 'l', 'o', 0};
+    cout << str1 << endl;
+    //cin >> str1;  // enter , tap , space bar 입력이 끝난것으로 간주
+    cin.getline(str1,6); 
+	cout << str1 << endl;
+
+
+	char* cPtr = strchr(str1, 'l');
+    cout << cPtr - &str1[0] << endl; /// 위치 파악 가능
+
+	시험 : strchr 함수 주고 인덱스 번호 찾는 코드 넣어라
+	cPtr - &str1[0] 작성
+
+	char
+	10-8번 스킵
+
+	strstr -> 문자열 2개가 같으면 0/ 아니면 1
+	같으면 0/ 틀리면 1 (앞에것이 크면 1 , 뒤에것이 크면 -1) -> 함수 만들어 보기
+
+	stringcat -> 함수 만들어 보기
+
+	
+    
+    return 0;
+
+	람다 + 영상 + c++string class -> 541p
+}
+	*/
+
+	//만들어 보기
+
+
+
