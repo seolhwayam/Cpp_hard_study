dfa3b459126426d4fb139f3edf96ecd90e2dc14c
study <susung462@gmail.com>
2023-05-10 01:40:58 +0900

    cpp_review_Week02_02(auto)

--- review/CPP_forouzan_teagan_review/CPP_forouzan_teagan_review/cpp_review_Week02_02(auto).cpp
+++ review/CPP_forouzan_teagan_review/CPP_forouzan_teagan_review/cpp_review_Week02_02(auto).cpp
@@ -9,6 +9,18 @@ int main() {
 		auto 배열(x) 문법적으로 배열은 auto처리 불가능
 		*/
 
+	auto f = 3.24f;
+	auto s = "test";
+	// 1) auto는 뒤의 값을 추론하여 해당 타입으로 실행
+
+	const auto a = 3.1f;
+	// 2) auto 키워드도 const가능
+
+	cout << f << endl;
+	cout << s << endl;
+
+	cout << sizeof(f) << "\t" << typeid(f).name() << endl;
+	cout << sizeof(s) << "\t" << typeid(s).name() << endl;
 
 
 	return 0;
