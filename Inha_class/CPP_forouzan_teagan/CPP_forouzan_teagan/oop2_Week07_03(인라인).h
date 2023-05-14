a3bac5833751a49115679c13b730e4f75ce21122
study <susung462@gmail.com>
2023-05-14 22:10:12 +0900

    test

--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week07_03(인라인).h
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week07_03(인라인).h
@@ -2,6 +2,6 @@
 class Test {
 public:
 	//static int a;
-	inline static int a; //속성 변경 해야됨
+	//inline static int a; //속성 변경 해야됨
 };
 //int Test::a = 10;
--- review/CPP_forouzan_teagan_review/CPP_forouzan_teagan_review/cpp_review_Week02_02(auto).cpp
+++ review/CPP_forouzan_teagan_review/CPP_forouzan_teagan_review/cpp_review_Week02_02(auto).cpp
@@ -1,27 +1,30 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-	/*auto
-	  : 자료타입이 들어가는 자리에 auto키워드 작성시 해당 auto키워들을 가진 변수와 뒤에서 대입이 되는 초기화되는 값들에 의해서
-	    추론을 해서 타입을 지정
-
-		auto 배열(x) 문법적으로 배열은 auto처리 불가능
-		*/
-
-	auto f = 3.24f;
-	auto s = "test";
-	// 1) auto는 뒤의 값을 추론하여 해당 타입으로 실행
-
-	const auto a = 3.1f;
-	// 2) auto 키워드도 const가능
-
-	cout << f << endl;
-	cout << s << endl;
-
-	cout << sizeof(f) << "\t" << typeid(f).name() << endl;
-	cout << sizeof(s) << "\t" << typeid(s).name() << endl;
-
-
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	/*auto
+//	  : 자료타입이 들어가는 자리에 auto키워드 작성시 해당 auto키워들을 가진 변수와 뒤에서 대입이 되는 초기화되는 값들에 의해서
+//	    추론을 해서 타입을 지정
+//
+//		auto 배열(x) 문법적으로 배열은 auto처리 불가능
+//
+//		기타 말씀
+//		1) 실수타입은 기본적으로 double로 auto 배정
+//		*/
+//
+//	auto f = 3.24f;
+//	auto s = "test";
+//	 //1) auto는 뒤의 값을 추론하여 해당 타입으로 실행
+//
+//	const auto a = 3.1f;
+//	 //2) auto 키워드도 const가능
+//
+//	cout << f << endl;
+//	cout << s << endl;
+//
+//	cout << sizeof(f) << "\t" << typeid(f).name() << endl;
+//	cout << sizeof(s) << "\t" << typeid(s).name() << endl;
+//
+//
+//	return 0;
+//}
