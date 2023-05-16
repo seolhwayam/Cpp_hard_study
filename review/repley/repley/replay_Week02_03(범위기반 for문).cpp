3c424878ab538e33ce3af5048b618a2f282f546a
study <susung462@gmail.com>
2023-05-16 23:20:50 +0900

    relpay_Week02-03(범위기간for문)

---
+++ review/repley/repley/replay_Week02_03(범위기반 for문).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Old 버전
+	/*
+	char s[] = { 'H','e','l','l','o' };
+	void* ps;
+	ps = s;
+	for (int i = 0; i < sizeof(s); i++)
+		cout << *((char*)ps + i);
+	*/
+
+	// Modern(범위기반)
+	char letters[] = "Hello";
+	void* ps;
+	ps = letters;
+
+	//ranged based for loop (범위기반 for문) 
+	for (char letter : letters)  //letters안에 있는 원소를 하나씩 꺼냄
+		cout << letter;
+
+	for (int i : {1, 3, 5, 7, -9})
+		cout << i;
+
+
+	return 0;
+}
