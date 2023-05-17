aa721a7b633c3342e1fecf340e86756dee8552a2
study <susung462@gmail.com>
2023-05-18 00:19:07 +0900

    repaly_Week03_02(구조체 포인터)

---
+++ review/replay/replay/replay_Week03_02(구초제 포인터).cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+struct Rectangle {
+	int x, y;
+	int w, h;
+};
+
+
+int main() {
+	Rectangle r = { 15,10,50,70 }; //r = 구조체 변수
+	Rectangle* pr = &r; //포인터 변수는 타입이 같아야 한다(타입 : Rectangle) + 구조체 변수의 주소(구조체 변수 r :: &r) 
+
+	cout << r.x << " " << (*pr).x << endl;
+	cout << (*pr).y << " " << pr->y << endl;
+	//(*pr).y =  pr->y  [화살표 연산자]
+	cout << (*pr).h << " " << pr->h << endl;
+	pr->h = 40; //화살표 연산자를 통한 값 변경
+	cout << (*pr).h << " " << pr->h << endl;
+
+	(*pr).w = 100;
+	cout << r.w << " " << pr->w << endl;
+
+	return 0;
+}
