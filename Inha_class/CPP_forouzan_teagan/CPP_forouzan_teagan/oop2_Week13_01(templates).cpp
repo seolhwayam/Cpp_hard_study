ffb69d513139659413f66cf33bd873215e0322cc
study <susung462@gmail.com>
2023-05-26 22:44:31 +0900

    oop2_Week13_01(템플릿)

--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week13_01(templates).cpp
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week13_01(templates).cpp
@@ -1,35 +1,35 @@
 //#include<iostream>
 //using namespace std;
-//
-//template <typename T, typename A, typename B, int MAX> // 템플릿
-////여러개 올 수 있다
-//
-//
-//class AutoArray {
-//public:
-//	AutoArray(T* ptr) {
-//		_ptr = ptr;
-//	}
-//	~AutoArray() {
-//		delete[]_ptr;
-//	}
-//	int& operator[] (int index) {
-//		return _ptr[index];
-//	}
-//private:
-//	T* _ptr;
-//};
-//
-//int main() {
-//	AutoArray<int> arri(new int[100]);
-//	arri[0] = 100;
-//
-//	AutoArray<float> arrf(new float[100]);
-//	arrf[99] = 2.71f;
-//
-//	AutoArray<short> arrs(new short[100]);
-//	arrs[7] = 2.71f;
-//	return 0;
-//
-//}
-///*잘 디자인 해놓으면 밑에서 쓰는 것은 클래스 재디자인없이 사용가능*/
+
+template <typename T> // 템플릿
+//여러개 올 수 있다
+
+
+class Autoarray {
+public:
+	Autoarray(T* ptr) {
+		_ptr = ptr;
+	}
+	~Autoarray() {
+		delete[]_ptr;
+	}
+	T& operator[] (int index) {
+		return _ptr[index];
+	}
+private:
+	T* _ptr;
+};
+
+int main() {
+	Autoarray<int> arri(new int[100]);
+	arri[0] = 100;
+
+	Autoarray<float> arrf(new float[100]);
+	arrf[99] = 2.71f;
+
+	Autoarray<short> arrs(new short[100]);
+	arrs[7] = 2.71;
+	return 0;
+
+}
+/*잘 디자인 해놓으면 밑에서 쓰는 것은 클래스 재디자인없이 사용가능*/
