4517b58c0c8bc5651d6fb9e2a9a16f3e45492f70
study <susung462@gmail.com>
2023-05-27 02:53:17 +0900

    oop2_Week13_03(입출력 조정자)

--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week13_02(STL-sort).cpp
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week13_02(STL-sort).cpp
@@ -1,7 +1,12 @@
 //#include<iostream>
+//#include<functional>
 //#include<algorithm> // 정렬, 검색등과 같은 알고리즘을 구현해놓은 함수들의 모음
+////sort(기본은 오름차순), find(선형검색 알고리즘 : 처음부터 검색) ,replace(찾기 -> 변경), reverse(반대로), binary_search 함수
+//
 //#include<list> // 링크드 리스트(삽입, 삭제에 강점) -> 업데이트 빈번할 시 유리
 //#include<vector> // 동적 배열(검색 위주에 강점 -> 업데이트 없을 시
+//// map, deque 공부하기
+//
 //using namespace std;
 //
 //bool myfunc(int i, int j) { return i > j; } //내림차순(true)
@@ -9,12 +14,12 @@
 ////bool myfunc(int i, int j) { return i < j; } //오름차순(false)
 //
 //
-//struct myStruct
+//struct mystruct
 //{
 //	bool operator() (int i, int j) { return (i > j); } //내림차순(true)
 //}myobject;
 //
-//class myClass
+//class myclass
 //{
 //public:
 //	bool operator() (int i, int j) { return (i > j); } //내림차순(true)
@@ -65,5 +70,21 @@
 //
 //	for (auto v : vec)
 //		cout << v << endl;
+//
+//	//④ #include<functional>
+//	sort(vec.begin(), vec.end(), greater<char>()); //내림차순
+//
+//	for (auto v : vec)
+//		cout << v << endl;
+//
+//	//⑤ 람다 함수(함수 확장)
+//	sort(vec.begin(), vec.end(), [](int i, int j) {return i > j; }); //내림차순
+//	// ♠ 람다 함수
+//	// 캡처리스트 [] + (매개변수 타입 변수이름 , 매개변수 타입 변수이름){return 리턴값;}
+//
+//	for (auto v : vec)
+//		cout << v << endl;
+//
+//
 //
 //}
