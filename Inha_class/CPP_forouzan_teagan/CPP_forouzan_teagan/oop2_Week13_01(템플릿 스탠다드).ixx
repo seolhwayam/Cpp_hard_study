0ec83fe428a3837251dbd47c27810eb4b7db2ca6
study <susung462@gmail.com>
2023-05-26 23:33:42 +0900

    oop2_Week13_01(STL LIKEDLIST)

---
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week13_01(템플릿 스탠다드).ixx
@@ -0,0 +1,3 @@
+﻿export module oop2_Week13_01_템플릿_스탠다드_;
+
+export void MyFunc();
--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week13_01(템플릿 함수).cpp
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week13_01(템플릿 함수).cpp
@@ -1,21 +1,21 @@
-#include<iostream>
-using namespace std;
-template<typename T>
-
-T max(T a, T b) {
-	return (a > b) ? a : b;
-
-}
-void main() {
-
-	int i1 = 7, i2 = 4;
-	int i3 = max(i1, i2);
-	cout << i3 << endl;
-
-	double d1 = 3.14, d2 = 2.71;
-	double d3 = max(d1, d2);
-	cout << d3 << endl;
-
-	return 0;
-}
-//클래스나 함수 자체를 내부적으로 만들어냄
+//#include<iostream>
+//using namespace std;
+//template<typename T>
+//
+//T max(T a, T b) {
+//	return (a > b) ? a : b;
+//
+//}
+//void main() {
+//	int i1 = 7, i2 = 4;
+//	int i3 = max(i1, i2);
+//	cout << i3 << endl;
+//
+//	double d1 = 3.14, d2 = 2.71;
+//	double d3 = max(d1, d2);
+//	cout << d3 << endl;
+//}
+////① 클래스나 함수 자체를 내부적으로 만들어냄
+////② 컴파일 시간에코드를 만들어 낸다. 소스코드를 컴파일하는 도중에 만들어 낸다라는 개념 이해
+////③ 템플릿 함수의 정의 구분은 헤더파일에 적어야 한다.(일반함수, 맴버함수 모두 포함)
+////④ 함수를 만든다 (x) , 컴퓨터한테 함수를 만드는 방법을 알려주는 것과 동일하다(o)
