1d31d78b742c71a2999a69d697b494e71ae18ab5
study <susung462@gmail.com>
2023-05-26 23:04:59 +0900

    oop2_Week13_01(템플릿 함수)

--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week13_01(templates).cpp
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week13_01(templates).cpp
@@ -1,35 +1,39 @@
-//#include<iostream>
-//using namespace std;
-
-template <typename T> // 템플릿
-//여러개 올 수 있다
-
-
-class Autoarray {
-public:
-	Autoarray(T* ptr) {
-		_ptr = ptr;
-	}
-	~Autoarray() {
-		delete[]_ptr;
-	}
-	T& operator[] (int index) {
-		return _ptr[index];
-	}
-private:
-	T* _ptr;
-};
-
-int main() {
-	Autoarray<int> arri(new int[100]);
-	arri[0] = 100;
-
-	Autoarray<float> arrf(new float[100]);
-	arrf[99] = 2.71f;
-
-	Autoarray<short> arrs(new short[100]);
-	arrs[7] = 2.71;
-	return 0;
-
-}
-/*잘 디자인 해놓으면 밑에서 쓰는 것은 클래스 재디자인없이 사용가능*/
+////#include<iostream>
+////using namespace std;
+//
+//template <typename t, typename A,int MAX> // 템플릿
+//// typename 여러개 가능, 고정된 값도 선언 가능
+//// 컴파일러가 동작
+//
+////여러개 올 수 있다
+//
+//
+//class autoarray {
+//public:
+//	autoarray(t* ptr) {
+//		_ptr = ptr;
+//	}
+//	~autoarray() {
+//		delete[]_ptr;
+//	}
+//	t& operator[] (int index) {
+//		return _ptr[index];
+//	}
+//private:
+//	t* _ptr;
+//};
+//
+//int main() {
+//	autoarray<int> arri(new int[100]);
+//	//autoarray타입(int) ptr(new int
+//	arri[0] = 100;
+//
+//	autoarray<float> arrf(new float[100]);
+//	arrf[99] = 2.71f;
+//
+//	autoarray<short> arrs(new short[100]);
+//	arrs[7] = 2.71;
+//	return 0;
+//
+//}
+///*잘 디자인 해놓으면 밑에서 쓰는 것은 클래스 재디자인없이 사용가능*/
--- Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week13_01(템플릿 함수).cpp
+++ Inha_class/CPP_forouzan_teagan/CPP_forouzan_teagan/oop2_Week13_01(템플릿 함수).cpp
@@ -1,19 +1,21 @@
-//#include<iostream>
-//using namespace std;
-//template<typename T>
-//
-//T max(T a, T b) {
-//	return (a > b) ? a : b;
-//
-//}
-//void main() {
-//
-//	int i1 = 7, i2 = 4;
-//	int i3 = max(i1, i2);
-//	cout << i3 << endl;
-//
-//	double d1 = 3.14, d2 = 2.71;
-//	double d3 = max(d1, d2);
-//	cout << d3 << endl;
-//}
-////클래스나 함수 자체를 내부적으로 만들어냄
+#include<iostream>
+using namespace std;
+template<typename T>
+
+T max(T a, T b) {
+	return (a > b) ? a : b;
+
+}
+void main() {
+
+	int i1 = 7, i2 = 4;
+	int i3 = max(i1, i2);
+	cout << i3 << endl;
+
+	double d1 = 3.14, d2 = 2.71;
+	double d3 = max(d1, d2);
+	cout << d3 << endl;
+
+	return 0;
+}
+//클래스나 함수 자체를 내부적으로 만들어냄
