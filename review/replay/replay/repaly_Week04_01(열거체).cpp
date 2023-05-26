1b634707f84201888df7c89aa7eccca168cba199
study <susung462@gmail.com>
2023-05-27 03:18:29 +0900

    repaly_Week04_01(열거체)

--- review/replay/replay/repaly_Week04_01(열거체).cpp
+++ review/replay/replay/repaly_Week04_01(열거체).cpp
@@ -1,19 +1,29 @@
-//#include<iostream>
-//using namespace std;
-//
-//enum{JOB_NECROMANCER, JOB_BABARIAN, JOB_WIZARD};
-//
-//struct Character {
-//	int jobType; //0 : 강령술사, 1: 야만 용사, 2: 마법사
-//};
-//
-//int main() {
-//	Character c;
-//	c.jobType = JOB_BABARIAN;
-//	if (c.jobType == JOB_BABARIAN) {
-//		cout << "비전격류!" << endl;
-//	}
-//
-//
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+
+enum{job_necromancer, job_babarian, job_wizard};
+/*
+* 
+* enum
+① 가독성↑
+② 수치의 의미
+*/
+
+struct character {
+	int jobtype; //0 : 강령술사, 1: 야만 용사, 2: 마법사
+};
+
+int main() {
+	character c;
+	c.jobtype = job_wizard;
+	if (c.jobtype == job_wizard) {
+		cout << "비전격류!" << endl;
+	}
+	c.jobtype = job_wizard;
+	if (c.jobtype == 2) {
+		cout << "비전격류!" << endl;
+	}
+
+
+	return 0;
+}
--- review/replay/replay/replay_Week03_02(구초제 포인터).cpp
+++ review/replay/replay/replay_Week03_02(구초제 포인터).cpp
@@ -33,34 +33,34 @@
 //
 //	return 0;
 //}
-#include<iostream>
-using namespace std;
-
-int main() {
-
-
-    cout << "2)char ╧Х©╜" << endl;
-    char bb[] = { 'a','b','c', 0};
-    char* pbb = bb;
-
-    cout << bb << endl;  //abc      
-    cout << *bb << endl; //ллллллллллллллллллллллллллллллллD?╜Z           
-    cout << &bb << endl; // a      
-    cout << bb + 1 << endl; // 0000005AAD9DFA44         
-    cout << bb + 5 << endl; // bc               
-    cout << *bb + 1 << endl; //ллллллллллллллллллллллллллллллллD?╜Z                      
-    cout << *bb + 5 << endl; //лллллллллллллллллллллллллллллл??╜Z                      
-    cout << *(bb + 1) << endl; // b                        b
-    cout << *(bb + 2) << endl; //c                        c
-
-    cout << pbb << endl; //abc
-    cout << *pbb << endl; // a
-    cout << &pbb << endl; // pbbюг аж╪р 0000005AAD9DFA70 
-    cout << &pbb + 1 << endl; // bc
-    cout << pbb + 1 << endl; // ллллллллллллллллллллллллллллллллD?╜Z
-    cout << *pbb + 1 << endl; // 98
-    cout << *(pbb + 1) << endl; //b
-    cout << pbb[2] << endl; //c
-
-    return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//
+//    cout << "2)char ╧Х©╜" << endl;
+//    char bb[] = { 'a','b','c', 0};
+//    char* pbb = bb;
+//
+//    cout << bb << endl;  //abc      
+//    cout << *bb << endl; //a          
+//    cout << &bb << endl; // 00000098B4AFF504     
+//    cout << bb + 1 << endl; // bc      
+//    cout << bb + 5 << endl; // лллллллллллллллллллллллллллллл?У╞╢≤           
+//    cout << *bb + 1 << endl; //98  [aюг ╬ф╫╨е╟ дз╣Е[97]+1 = 98]                   
+//    cout << *bb + 5 << endl; //102                     
+//    cout << *(bb + 1) << endl; // b                        
+//    cout << *(bb + 2) << endl; //c                        
+//
+//    cout << pbb << endl; //abc
+//    cout << *pbb << endl; // a
+//    cout << &pbb << endl; //00000098B4AFF528
+//    cout << &pbb + 1 << endl; //00000098B4AFF530
+//    cout << pbb + 1 << endl; // bc
+//    cout << *pbb + 1 << endl; // 98
+//    cout << *(pbb + 1) << endl; //b
+//    cout << pbb[2] << endl; //c
+//
+//    return 0;
+//}
