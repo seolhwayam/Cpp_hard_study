a1db752cc49b84f18507358fb879a253cb2b47c4
study <susung462@gmail.com>
2023-05-27 21:46:22 +0900

    repaly_Week04_03(포인터 함수)

---
+++ review/replay/replay/repaly_Week04_03(포인터 함수).cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+int Factorial(int);
+int FactorialRecursion(int);
+/*
+함수 포인터
+1. 리턴타입(* 함수 포인터 이름)(매개변수 타입)
+2. 함수의 리턴타입과 매개변수 타입이 맞아야 사용 가능
+함수 : int Factorial(int);      [리턴타입 int  매개변수 타입 int]
+포인터 함수 : int(*pFact)(int); [리턴타입 int  매개변수 타입 int]
+3. 포인터는 주소를 받는다 -> 함수포인터는 함수의 주소를 받는다 -> & 함수이름
+
+[사용법]
+1) 함수 포인터 생성 :  리턴타입(* 함수 포인터 이름)(매개변수 타입)
+2) 함수 포인터 초기화 : 함수 포인터 이름 = &함수이름
+3) 사용하기 : (*함수 포인터 이름)(매개변수)
+*/ 
+
+int main() {
+
+	int in;
+	cin >> in;
+
+	int(*pFact)(int);
+
+	pFact = &Factorial; 
+	/*
+	*포인터는 주소를 받는다 -> 함수포인터는 함수의 주소를 받는다 -> & 함수이름
+	* 리턴타입, 매개변수 타입 매칭
+	*/
+
+	cout << (*pFact)(in) << endl;
+
+	cout << Factorial(in) << endl;
+	cout << FactorialRecursion(in) << endl;
+
+	return 0;
+}
+
+
+int FactorialRecursion(int f) {
+	if (f <= 1)
+		return 1;
+	else
+		return f * FactorialRecursion(f - 1);
+}
+
+int Factorial(int f) {
+	int result = 1;
+
+	for (int i = 1; i <= f; i++) {
+		result = result * i;
+	}
+	return result;
+}
+
+
--- review/replay/replay/replay_Week04_03(함수_팩토리얼,프로토 타입).cpp
+++ review/replay/replay/replay_Week04_03(함수_팩토리얼,프로토 타입).cpp
@@ -1,64 +1,64 @@
-#include<iostream>
-using namespace std;
-/*
-함수
-1. 기능
-
-*/
-
-void Hello(int);  // 인사하는 함수 / 매개변수 타입만 적어줘도 됨!
-
-int Factorial(int); // 팩토리얼 함수, 반복문 이용
-
-int FactorialRecursion(int); // 팩토리얼 함수, 재귀함수 이용
-
-
-/*프로토 타입[리턴 타입 함수이름(매개변수); ]
-* 1. 프로토 타입 // 설명 -> 무슨 기능인지 쉽게 확인
-* */
-
-
-int main() {
-	int in;
-	cin >> in;
-
-	cout << Factorial(in) << endl;
-	cout << FactorialRecursion(in) << endl;
-	Hello(5); // 함수 호출
-	
-	return 0;
-}
-
-int Factorial(int f) {
-	int result = 1;
-
-	for (int i = 1; i <= f; i++) {
-		result = result * i;
-	}
-	return result;
-
-}
-
-int FactorialRecursion(int f) {
-	//① f = 4
-	//③ f = 3
-	//⑤ f = 2
-	//⑥ f = 1
-	if (f <= 1)
-		//⑦ f=1이므로 return 1로 종료
-		return 1;
-	else
-		return f * FactorialRecursion(f - 1); //⑪ return 24
-	//②       4* FactorialRecursion(3)  ⑩ 24(4*6)  
-	//④       3* FactorialRecursion(2)  ⑨ 6(2*3)  
-	//⑤       2* FactorialRecursion(1)  ⑧ 2(2*1)  
-	
-}
-
-
-
-void Hello(int n) {
-
-	cout << "HI~" << n << endl;
-
-}
+//#include<iostream>
+//using namespace std;
+///*
+//함수
+//1. 기능
+//
+//*/
+//
+//void Hello(int);  // 인사하는 함수 / 매개변수 타입만 적어줘도 됨!
+//
+//int Factorial(int); // 팩토리얼 함수, 반복문 이용
+//
+//int FactorialRecursion(int); // 팩토리얼 함수, 재귀함수 이용
+//
+//
+///*프로토 타입[리턴 타입 함수이름(매개변수); ]
+//* 1. 프로토 타입 // 설명 -> 무슨 기능인지 쉽게 확인
+//* */
+//
+//
+//int main() {
+//	int in;
+//	cin >> in;
+//
+//	cout << Factorial(in) << endl;
+//	cout << FactorialRecursion(in) << endl;
+//	Hello(5); // 함수 호출
+//	
+//	return 0;
+//}
+//
+//int Factorial(int f) {
+//	int result = 1;
+//
+//	for (int i = 1; i <= f; i++) {
+//		result = result * i;
+//	}
+//	return result;
+//
+//}
+//
+//int FactorialRecursion(int f) {
+//	//① f = 4
+//	//③ f = 3
+//	//⑤ f = 2
+//	//⑥ f = 1
+//	if (f <= 1)
+//		//⑦ f=1이므로 return 1로 종료
+//		return 1;
+//	else
+//		return f * FactorialRecursion(f - 1); //⑪ return 24
+//	//②       4* FactorialRecursion(3)  ⑩ 24(4*6)  
+//	//④       3* FactorialRecursion(2)  ⑨ 6(2*3)  
+//	//⑤       2* FactorialRecursion(1)  ⑧ 2(2*1)  
+//	
+//}
+//
+//
+//
+//void Hello(int n) {
+//
+//	cout << "HI~ " << n << endl;
+//
+//}
