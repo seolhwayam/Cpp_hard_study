31683056d0a00997bf320b1b3763aa2b5088827f
study <susung462@gmail.com>
2023-05-28 15:02:11 +0900

    repaly_Week07_01(클래스와 객체)

--- review/replay/replay/repaly_Week06_03(분리).cpp
+++ review/replay/replay/repaly_Week06_03(분리).cpp
@@ -1,13 +1,31 @@
-#include<iostream>
-#include "ex1(repaly_Week06_03(분리).h"
-using namespace std;
-
-
-int main() {
-	int r;
-	r = Plus(3, 7);
-	cout << r << endl;
-
-
-	return 0;
-}
+//#include<iostream>
+//#include "ex1(repaly_Week06_03(분리).h"
+//using namespace std;
+///* [파일 분리하는 방법]
+//cpp파일 : 함수의 원본
+//h파일 : 함수의 프로토 타입
+//main파일 : #include "h파일"
+//
+//※ cpp파일이름 = h파일이름 (★동일 해야한다)
+//=========================================
+//cpp파일 : 함수의 원본
+//int Plus(int a, int b) {
+//	return a + b;
+//}
+//
+//h파일 : 함수의 프로토 타입
+//int Plus(int a, int b);
+//
+//main파일 : #include "h파일"
+//#include "ex1(h파일).h"
+//
+//
+//*/
+//
+//int main() {
+//	int r;
+//	r = Plus(3, 7);
+//	cout << r << endl;
+//
+//	return 0;
+//}
